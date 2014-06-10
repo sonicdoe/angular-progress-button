9d01caeb5300127b0aec8d566d0d6fd1435a19ef
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2014-06-10 15:24:07 +0200

    Implement simulateProgress() for example page

--- example/script.js
+++ example/script.js
@@ -1,5 +1,22 @@
 angular.module('progressButtonDemo', ['progressButton'])
-	.controller('ProgressButtonsCtrl', function($scope) {
+	.controller('ProgressButtonsCtrl', function($scope, $interval) {
+		$scope.simulateProgress = function(buttonIndex, seconds, callback) {
+			if($scope.buttons[buttonIndex].simulating) return
+
+			$scope.buttons[buttonIndex].simulating = true
+			$scope.buttons[buttonIndex].value = 0.2
+
+			var interval = $interval(function() {
+				$scope.buttons[buttonIndex].value += 0.2
+
+				if($scope.buttons[buttonIndex].value >= 1.0) {
+					$interval.cancel(interval)
+
+					if(typeof callback === 'function') callback()
+				}
+			}, (seconds / 5) * 1000)
+		}
+
 		$scope.generate = function(button) {
 			button.progressTimed(3, function() {
 				$scope.generate = function() {
