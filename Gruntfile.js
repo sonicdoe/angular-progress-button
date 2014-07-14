439f689096d46eac35c2adfe23b901e220a0f558
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2014-07-14 15:00:02 +0200

    Add cssmin to Grunt task ‘build’

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,15 @@ module.exports = function(grunt) {
 				browsers: ['PhantomJS']
 			}
 		},
+		cssmin: {
+			build: {
+				options: {
+					banner: '/* <%= bowerPkg.name %> v<%= bowerPkg.version %> (<%= bowerPkg.homepage %>) */'
+				},
+				src: 'src/progress-button.css',
+				dest: 'dist/progress-button.min.css'
+			}
+		},
 		uglify: {
 			options: {
 				banner: '/* <%= bowerPkg.name %> v<%= bowerPkg.version %> (<%= bowerPkg.homepage %>) */\n'
@@ -19,10 +28,11 @@ module.exports = function(grunt) {
 		}
 	})
 
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify')
 	grunt.loadNpmTasks('grunt-karma')
 
-	grunt.registerTask('build', ['uglify'])
+	grunt.registerTask('build', ['cssmin', 'uglify'])
 	grunt.registerTask('test', ['karma'])
 	grunt.registerTask('default', [])
 }
