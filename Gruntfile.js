77993be882083150ca4ae36452c569d702f77fe2
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2014-07-14 14:39:26 +0200

    Add Grunt task ‘test’

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,17 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		bowerPkg: grunt.file.readJSON('bower.json')
+		bowerPkg: grunt.file.readJSON('bower.json'),
+		karma: {
+			unit: {
+				configFile: 'karma.conf.js',
+				singleRun: true,
+				browsers: ['PhantomJS']
+			}
+		}
 	})
 
+	grunt.loadNpmTasks('grunt-karma')
+
+	grunt.registerTask('test', ['karma'])
 	grunt.registerTask('default', [])
 }
