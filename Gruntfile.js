ae4fffbd506bc5dfd75d207815ade21df66fefc7
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2014-07-14 14:53:56 +0200

    Add Grunt task ‘build’

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,11 +7,22 @@ module.exports = function(grunt) {
 				singleRun: true,
 				browsers: ['PhantomJS']
 			}
+		},
+		uglify: {
+			options: {
+				banner: '/* <%= bowerPkg.name %> v<%= bowerPkg.version %> (<%= bowerPkg.homepage %>) */\n'
+			},
+			build: {
+				src: 'src/progress-button.js',
+				dest: 'dist/progress-button.min.js'
+			}
 		}
 	})
 
+	grunt.loadNpmTasks('grunt-contrib-uglify')
 	grunt.loadNpmTasks('grunt-karma')
 
+	grunt.registerTask('build', ['uglify'])
 	grunt.registerTask('test', ['karma'])
 	grunt.registerTask('default', [])
 }
